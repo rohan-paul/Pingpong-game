d41079a08aceec4022d280504d3bb6a0d2ce94df
Rohan Paul <rohanpaul2@gmail.com>
2016-05-06 16:30:53 +0530

    Finalizing the game

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
-A small HTML5 and Javascript based game.
+A very small HTML5 and Javascript based game.
 
-To play the game run ``git clone https://github.com/rohan-paul/Pingpong-game.git`` and then just open the index.html file with a browser
+To play the game run ``git clone https://github.com/rohan-paul/Pingpong-game.git`` and then just open the index.html file with a browser.
--- js/pingpong.js
+++ js/pingpong.js
@@ -23,11 +23,13 @@
             speed: 5, // Speed defines how many pixels the ball moves in each step
             x: 150,
             y: 100,
-            redius: 20,
+            radius: 20,
             directionX: 1,
             directionY: 1
-        }
+        },
         //  When the directionX/Y value is 1, the ball moves to the positive direction of the axis. When the direction is -1, the ball moves to the negative direction. By toggling the x and y directions, we can move the ball in four directions.
+        scoreA : 0, // score for player A
+        scoreB : 0 // score for player B
     };
 
     // Ball collision logic
@@ -44,7 +46,7 @@
         return pingpong.ball.x + pingpong.ball.speed * pingpong.ball.directionX < 0;
     }
 
-        // Ball movement logic
+    // Ball movement logic
     function moveBall() {
         // reference useful variables
         var ball = pingpong.ball;
@@ -64,7 +66,24 @@
             playerBWin();
         }
 
-        // check paddles here
+        // variables for checking paddles
+        ballX = ball.x + ball.speed * ball.directionX;
+        ballY = ball.y + ball.speed * ball.directionY;
+
+        /*check collision detection between the ball and left paddle. If the ball's x position is in between paddleA's left and right boundary (i.e. paddleA's (x position and (x position + width) AND ALSO the ball's y position is inbetween paddleA's bottom and top boundary - ONLY then a collision is being detected.*/
+
+        if (ballX >= pingpong.paddleA.x && ballX < pingpong.paddleA.x + pingpong.paddleA.width) {
+            if (ballY <= pingpong.paddleA.y + pingpong.paddleA.height && ballY >= pingpong.paddleA.y) {
+                ball.directionX = 1;
+            }
+        }
+
+        // check right paddle
+        if (ballX + pingpong.ball.radius >= pingpong.paddleB.x && ballX < pingpong.paddleB.x + pingpong.paddleB.width) {
+            if (ballY <= pingpong.paddleB.y + pingpong.paddleB.height && ballY >= pingpong.paddleB.y) {
+                ball.directionX = -1;
+            }
+        }
 
         // update the ball position data. This will effectively, move the ball 5 pixel at a time.
         ball.x += ball.speed * ball.directionX;
@@ -78,6 +97,8 @@
 
         // Update the ball location variable
         pingpong.ball.directionX = -1;
+        pingpong.scoreA += 1;
+        $("#score-a").text(pingpong.scoreA);
     }
 
     function playerBWin() {
@@ -86,6 +107,20 @@
 
         // Update the ball location variable
         pingpong.ball.directionX = 1;
+        pingpong.scoreB += 1;
+        $("#score-b").text(pingpong.scoreB);
+    }
+
+    // function to automatically move the left paddle
+    function autoMovePaddleA() {
+        var speed = 4;
+        var direction = 1;
+
+        var paddleY = pingpong.paddleA.y + pingpong.paddleA.height / 2;
+        if (paddleY > pingpong.ball.y) {
+            direction = -1;
+        }
+        pingpong.paddleA.y += speed * direction;
     }
 
     // view rendering
@@ -96,31 +131,31 @@
 
     // After ball's movement is calculated, we want to render the view to update the ball's position based on the data.
     function renderBall() {
-    var ball = pingpong.ball;
-    $("#ball").css({
-      "left" : ball.x + ball.speed * ball.directionX,
-      "top" : ball.y + ball.speed * ball.directionY
-    });
-  }
+        var ball = pingpong.ball;
+        $("#ball").css({
+            "left": ball.x + ball.speed * ball.directionX,
+            "top": ball.y + ball.speed * ball.directionY
+        });
+    }
 
 
     // view inputs
     function handleMouseInputs() {
-    // run the game when mouse moves in the playground.
-    $('#playground').mouseenter(function(){
-      pingpong.isPaused = false;
-    });
+        // run the game when mouse moves in the playground.
+        $('#playground').mouseenter(function() {
+            pingpong.isPaused = false;
+        });
 
-    // pause the game when mouse moves out the playground.
-    $('#playground').mouseleave(function(){
-      pingpong.isPaused = true;
-    });
+        // pause the game when mouse moves out the playground.
+        $('#playground').mouseleave(function() {
+            pingpong.isPaused = true;
+        });
 
-    // calculate the paddle position by using the mouse position.
-    $('#playground').mousemove(function(e){
-      pingpong.paddleB.y = e.pageY - pingpong.playground.offsetTop;
-    });
-  }
+        // calculate the paddle position by using the mouse position.
+        $('#playground').mousemove(function(e) {
+            pingpong.paddleB.y = e.pageY - pingpong.playground.offsetTop;
+        });
+    }
 
     // Browser render loop
     function render() {
@@ -132,12 +167,13 @@
     // Define a gameloop function to move the ball on each game loop iteration.
     function gameloop() {
         moveBall();
+        autoMovePaddleA();
     }
 
     // starting point of entire game
     function init() {
         // Set interval to call gameloop logic in 30 FPS so that the ball moves every 33.3 milliseconds.
-        pingpong.timer = setInterval(gameloop, 1000/30);
+        pingpong.timer = setInterval(gameloop, 1000 / 30);
 
         // view rendering
         window.requestAnimationFrame(render);
